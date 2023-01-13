c8a4086339c6514f63f5743629dd695873c28bf2
Hunter Hardin <hunter.hardin@autopoint.com>
2023-01-12 19:49:48 -0500

    stock app 2

---
+++ node_app/app.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { engine } from 'express-handlebars';
+
+const app = express();
+
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+app.set('views', './views');
+
+const otherstuff = "hello there, this is the other stuff!";
+
+app.get('/', (req, res) => {
+    res.render('home', {
+		stuff: otherstuff
+	});
+});
+
+app.listen(3000);
